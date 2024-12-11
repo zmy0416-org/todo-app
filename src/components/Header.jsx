8a230b556f32e9763728098c0464dbfa50838708
zmy <123053563+zmy20220416@users.noreply.github.com>
2024-12-11 09:21:01 +0800

    fix: 展示 open task

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,10 @@
 import { useTodoContext } from "../context/todo-context"
 export default function Header() {
-  const { total } = useTodoContext()
-  const taskOrTasks = total <= 1 ? 'task' : 'tasks'
+  const { openCount } = useTodoContext()
+  const taskOrTasks = openCount <= 1 ? 'task' : 'tasks'
   return (
     <header>
-      <h1 className="text-gradient">You have {total} open {taskOrTasks}</h1>
+      <h1 className="text-gradient">You have {openCount} open {taskOrTasks}</h1>
     </header>
   )
 }
